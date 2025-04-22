fc1a5d571ca7321820fd3b9d7739f2372a79cf8f
StormGab1 <168318786+StormGab1@users.noreply.github.com>
2025-04-21 22:28:10 -0300

    Update and rename exec to exec.py

--- exec.py
+++ exec.py
@@ -1,11 +1,11 @@
 import re
 
-palavras_proibidas = ["carlos", "templeuser"]
- removidos = []
+while True:
+ palavras_proibidas = ["carlos", "templeuser"]
  
  for group_name in list(groups.keys()):
      for palavra in palavras_proibidas:
          if re.search(palavra, group_name, re.IGNORECASE):
-             removidos.append(group_name)
              del groups[group_name]
              break
+ time.sleep(50)
